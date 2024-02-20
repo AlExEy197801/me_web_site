d67ee09921b40f0c862fb4e136ab246f6dde145c
Алексей Трудов <atrudoff@yandex.by>
2024-02-20 20:29:57 +0300

    сделан вход и регистрация

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,12 @@
 from django.contrib.auth import login
 from django.urls import path, include
 from . import views
-from .views import MainPage #, RegisterUser, LoginUser
+from .views import MainPage , RegisterUser, LoginUser
 
 urlpatterns = [
-    path('', MainPage.as_view(), name='home')
+    path('', MainPage.as_view(), name='home'),
+    path('register/', RegisterUser.as_view(), name='registration'),
+    path('login/', LoginUser.as_view(), name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('activate/<str:uidb64>/<str:token>/', views.activate, name='activate'),
 ]
